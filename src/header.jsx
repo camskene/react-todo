67726c38ca935a1762e9d57a789b00603cb739d6
Cam Skene <camskene@gmail.com>
2015-09-12 01:02:17 -0400

    add text input to header

--- src/header.jsx
+++ src/header.jsx
@@ -1,15 +1,34 @@
 var React = require('react');
 
 var Header = React.createClass({
+  getInitialState: function() {
+    return {
+      text: ''
+    }
+  },
   render: function() {
     return <div className="input-group">
-      <input type="text" className="form-control" />
+      <input
+        value={this.state.text}
+        onChange={this.handleInputChange}
+        type="text"
+        className="form-control" />
       <span className="input-group-btn">
-        <button className="btn btn-default" type="button">
+        <button
+          onClick={this.handleClick}
+          className="btn btn-default"
+          type="button">
           Add
         </button>
       </span>
     </div>
+  },
+  handleClick: function() {
+    // send value of text input toe Firebase
+    console.log(this.state.text)
+  },
+  handleInputChange: function(event) {
+    this.setState({text: event.target.value})
   }
 });
 
