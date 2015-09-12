43d06a36167c416ef0be0cc111106e4c47fc98a0
Cam Skene <camskene@gmail.com>
2015-09-12 00:32:18 -0400

    setup firebase

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,20 @@
 var React = require('react');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var rootUrl = 'https://fiery-fire-7124.firebaseio.com/';
 
-var Hello = React.createClass({
+var App = React.createClass({
+  mixins: [ReactFire], // copy React methods to our component
+  componentWillMount: function() { // fired once before initial render
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+  },
   render: function() {
+    console.log(this.state);
     return <h1 className="red">
       Hello ReactJS!
     </h1>
   }
 });
 
-var element = React.createElement(Hello, {});
+var element = React.createElement(App, {});
 React.render(element, document.querySelector('.container'));
