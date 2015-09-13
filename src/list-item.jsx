161cc620de129fecc3f4017c00e3641487bdcc7a
Cam Skene <camskene@gmail.com>
2015-09-13 14:22:32 -0400

    hook up checkbox event handler

--- src/list-item.jsx
+++ src/list-item.jsx
@@ -1,16 +1,25 @@
 var React = require('react');
+var Firebase = require('firebase');
+var rootUrl = 'https://fiery-fire-7124.firebaseio.com/';
 
 var ListItem = React.createClass({
   getInitialState: function() {
     return {
-      text: this.props.item.text
+      text: this.props.item.text,
+      done: this.props.item.done
     }
   },
+  componentWillMount: function() {
+    this.fb = new Firebase(rootUrl + 'items/' + this.props.item.key)
+  },
   render: function() {
-
     return <div className="input-group">
       <span className="input-group-addon">
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={this.state.done}
+          onChange={this.handleDoneChange}
+          />
       </span>
       <input type="text"
         className="form-control"
@@ -20,6 +29,11 @@ var ListItem = React.createClass({
         <button className="btn btn-default">Delete</button>
       </span>
     </div>
+  },
+  handleDoneChange: function(event) {
+    var update = {done: event.target.checked};
+    this.setState(update);
+    this.fb.update(update);
   }
 });
 
