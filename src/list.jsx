9a5f97127b3e342b45129a78905b80c09b23c7a1
Cam Skene <camskene@gmail.com>
2015-09-13 13:56:00 -0400

    add list-item component

--- src/list.jsx
+++ src/list.jsx
@@ -1,11 +1,12 @@
 var React = require('react');
+var ListItem = require('./list-item');
 
 var List = React.createClass({
   render: function() {
     console.log(this.props);
-    return <ul>
+    return <div>
         {this.renderList()}
-      </ul>
+      </div>
   },
   renderList: function() {
     if ( this.props.items && Object.keys(this.props.items).length === 0 ) {
@@ -15,10 +16,13 @@ var List = React.createClass({
     } else {
       var children = [];
       for(var key in this.props.items) {
+        var item = this.props.items[key];
+        item.key = key;
         children.push(
-          <li>
-            {this.props.items[key].text}
-          </li>
+          <ListItem
+            item={item}
+            key={key}
+          />
         )
       }
       return children;
