d1ba27bbde4928ee0f78251c427b4a2fba4e0d28
Cam Skene <camskene@gmail.com>
2017-07-23 19:18:13 -1000

    Lint gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,45 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var source = require('vinyl-source-stream');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var reactify = require('reactify');
-var notifier = require('node-notifier');
-var server = require('gulp-server-livereload');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const source = require('vinyl-source-stream');
+const browserify = require('browserify');
+const watchify = require('watchify');
+const reactify = require('reactify');
+const notifier = require('node-notifier');
+const server = require('gulp-server-livereload');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const watch = require('gulp-watch');
 
-var notify = function(error) {
-  var message = 'In: ';
-  var title = 'Error: ';
+const notify = function (error) {
+  let message = 'In: ';
+  let title = 'Error: ';
 
-  if(error.description) {
+  if (error.description) {
     title += error.description;
   } else if (error.message) {
     title += error.message;
   }
 
-  if(error.filename) {
-    var file = error.filename.split('/');
-    message += file[file.length-1];
+  if (error.filename) {
+    const file = error.filename.split('/');
+    message += file[file.length - 1];
   }
 
-  if(error.lineNumber) {
-    message += '\nOn Line: ' + error.lineNumber;
+  if (error.lineNumber) {
+    message += `\nOn Line: ${error.lineNumber}`;
   }
 
-  notifier.notify({title: title, message: message});
+  notifier.notify({ title, message });
 };
 
-var bundler = watchify(browserify({
+const bundler = watchify(browserify({
   entries: ['./src/app.jsx'],
   transform: [reactify],
   extensions: ['.jsx'],
   debug: true,
   cache: {},
   packageCache: {},
-  fullPaths: true
+  fullPaths: true,
 }));
 
 function bundle() {
@@ -47,32 +47,32 @@ function bundle() {
     .bundle()
     .on('error', notify)
     .pipe(source('main.js'))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./'));
 }
-bundler.on('update', bundle)
+bundler.on('update', bundle);
 
-gulp.task('build', function() {
-  bundle()
+gulp.task('build', () => {
+  bundle();
 });
 
-gulp.task('serve', function(done) {
+gulp.task('serve', (done) => {
   gulp.src('')
     .pipe(server({
       livereload: {
         enable: true,
-        filter: function(filePath, cb) {
-          if(/main.js/.test(filePath)) {
-            cb(true)
-          } else if(/style.css/.test(filePath)){
-            cb(true)
+        filter(filePath, cb) {
+          if (/main.js/.test(filePath)) {
+            cb(true);
+          } else if (/style.css/.test(filePath)) {
+            cb(true);
           }
-        }
+        },
       },
-      open: true
+      open: true,
     }));
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', () => {
   gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('style.css'))
@@ -81,6 +81,6 @@ gulp.task('sass', function () {
 
 gulp.task('default', ['build', 'serve', 'sass', 'watch']);
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
