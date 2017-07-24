be74f2574e68cfd1089a3a77e2fb2fe1804a39d5
Cam Skene <camskene@gmail.com>
2017-07-23 19:23:00 -1000

    Prefer Object.{keys, etc} over for in loops.

--- src/list.jsx
+++ src/list.jsx
@@ -1,33 +1,22 @@
-var React = require('react');
-var ListItem = require('./list-item');
+const React = require('react');
+const ListItem = require('./list-item');
 
-var List = React.createClass({
-  render: function() {
-    console.log(this.props);
-    return <div>
-        {this.renderList()}
-      </div>
-  },
-  renderList: function() {
+const List = React.createClass({
+  render() {
     if (!this.props.items) {
-      return <h4>
-        Add a todo to get started
-      </h4>
-    } else {
-      var children = [];
-      for(var key in this.props.items) {
-        var item = this.props.items[key];
-        item.key = key;
-        children.push(
-          <ListItem
-            item={item}
-            key={key}
-          />
-        )
-      }
-      return children;
+      return <h4>Add a todo to get started</h4>;
     }
-  }
+
+    return (
+      <div>
+        {Object.keys(this.props.items).map((key) => {
+          const item = this.props.items[key];
+          item.key = key;
+          return <ListItem item={item} key={key} />;
+        })}
+      </div>
+    );
+  },
 });
 
 module.exports = List;
